2b064dbb362fbf6f4c8f4c6a13ab83d330f29b2b
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-09-16 02:53:18 -0400

    Background IMage added and opacity/ aplha set to 0.47 equivalent to 120 that was required. Using 120/255 we get 0.47

--- app/src/main/java/com/adefemikolawole/grizzlycolorapp/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/grizzlycolorapp/MainActivity.java
@@ -1,9 +1,18 @@
 package com.adefemikolawole.grizzlycolorapp;
 
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+final String txtAbout = "Author: Adefemi Kolawole";
+
+ConstraintLayout mainLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        print(txtAbout);
+mainLayout = (ConstraintLayout) findViewById(R.id.mainConstraintLayout);
+mainLayout.getBackground().setAlpha(120);
     }
+
+   public void print(String str){
+        System.out.print(str);
+   }
 }

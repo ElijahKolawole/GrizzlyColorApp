4e40af9c2f2096c9c42421a7bfa5b243df901e26
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-09-16 13:46:28 -0400

    swatch background transparencsy set to alpha:120 and spacing , padding, and margin also adjusted

--- app/src/main/java/com/adefemikolawole/grizzlycolorapp/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/grizzlycolorapp/MainActivity.java
@@ -4,15 +4,17 @@ import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-final String txtAbout = "Author: Adefemi Kolawole";
-
+final String txtAbout = "Author: Adefemi Kolawole\nClass: ITEC 4550\nDate: Fall 2018 (9/16/2018)";
 ConstraintLayout mainLayout;
 
 TextView txtSwatch;
+Button btAbout;
 
 
     @Override
@@ -31,9 +33,30 @@ TextView txtSwatch;
 //Set transparency for swatch view
         txtSwatch = (TextView) findViewById(R.id.txtSwatch);
         txtSwatch.getBackground().setAlpha(120);
+
+        //set Toast function for button about
+        btAbout = (Button) findViewById(R.id.btAbout);
+        btAbout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showAboutToast();
+            }
+        });
     }
 
    public void print(String str){
         System.out.print(str);
    }
+
+   public void showAboutToast(){
+
+       Toast.makeText(MainActivity.this, txtAbout, Toast.LENGTH_LONG).show();
+   }
+
+  /*  public void showAboutSnackbar(){
+SnackBar snackbar;
+      Snackbar.make(MainActivity.this, txtAbout, Toast.LENGTH_LONG).show();
+    }
+*/
 }
